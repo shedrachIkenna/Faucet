69b6af600c7454c995cb2fe58330f7fc43392a3f
shedrachIkenna <shedrach686@gmail.com>
2023-03-18 09:44:33 +0100

    Setup connect button with connectKit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { configureChains, createClient, WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, polygon } from 'wagmi/chains'
-import HomePage from './components/HomePage'
+import { WagmiConfig, createClient } from "wagmi";
+import { ConnectKitProvider, ConnectKitButton, getDefaultClient } from "connectkit";
+import HomePage from "./components/HomePage";
 
+const alchemyId = process.env.REACT_APP_ALCHEMY_ID;
 
-function App() {
-  const chains = [arbitrum, mainnet, polygon]
-  const projectId = process.env.REACT_APP_PROJECT_ID;
+const client = createClient(
+  getDefaultClient({
+    appName: "Dapp",
+    alchemyId,
+  }),
+);
 
-  const { provider } = configureChains(chains, [w3mProvider({ projectId })])
-  const wagmiClient = createClient({
-    autoConnect: true,
-    connectors: w3mConnectors({ projectId, version: 2, chains }),
-    provider
-  })
-  const ethereumClient = new EthereumClient(wagmiClient, chains)
-
+const App = () => {
   return (
-    <>
-      <WagmiConfig client={wagmiClient}>
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
         <HomePage />
-      </WagmiConfig>
+        <ConnectKitButton />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
+};
 
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-    </>
-  )
-}
-
-export default App;
+export default App
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,7 @@
-import { Web3Button } from '@web3modal/react'
 const HomePage = () => {
     return(
         <div>
             <h1>This is the home page</h1>
-            <Web3Button />
         </div>
     )
 }
